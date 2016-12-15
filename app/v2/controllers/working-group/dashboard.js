e1f6d82c166d7a25b7b58ef5c12a37f27393c7b9
Cristhian Parra <cdparra@gmail.com>
2016-12-15 13:29:33 -0800

    Created testing branch to enable some functionalities that we currently have hardcoded

--- app/v2/controllers/working-group/dashboard.js
+++ app/v2/controllers/working-group/dashboard.js
@@ -20,8 +20,8 @@
       // if the param is uuid then it is an anonymous user
       $scope.isAnonymous = false;
       // TODO: read the following from configurations in the campaign/component
-      $scope.newProposalsEnabled = false;
-      $scope.newIdeasEnabled = false;
+      $scope.newProposalsEnabled = true;
+      $scope.newIdeasEnabled = true;
       var pattern = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
       if (pattern.test($stateParams.gid)) {
         $scope.groupID = $stateParams.gid;
